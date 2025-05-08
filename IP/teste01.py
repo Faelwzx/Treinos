c0a02b52f9f4b81d4642bfcef2e4626b9889c8a7
Rafael de Oliveira <rafael.marques@aluno.ifce.edu.br>
2025-05-08 08:30:10 -0300

    Update teste01.py

--- IP/teste01.py
+++ IP/teste01.py
@@ -1,7 +1,12 @@
-# multiplicação
+## exercicio Exibindo resultado de tabuada de 9 formatada
 
-N1 = int(input("digite o primeiro número: "))
-N2 = int(input("digite segundo número: "))
-Resultado = N1 * N2
-
-print(f"o resultado é {Resultado}")
+print(f"9 x  1 = {9*1:2}")
+print(f"9 x  2 = {9*2:2}")
+print(f"9 x  3 = {9*3:2}")
+print(f"9 x  4 = {9*4:2}")
+print(f"9 x  5 = {9*5:2}")
+print(f"9 x  6 = {9*6:2}")
+print(f"9 x  7 = {9*7:2}")
+print(f"9 x  8 = {9*8:2}")
+print(f"9 x  9 = {9*9:2}")
+print(f"9 x 10 = {9*10:2}")
